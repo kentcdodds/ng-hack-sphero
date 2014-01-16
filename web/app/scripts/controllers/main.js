51fdc32a4da8c26d7b9773e8c3648a79a3f3db77
Jonathan Brown <jonnydangerous@gmail.com>
2014-01-15 21:24:52 -0700

    adding code to retrieve robot name

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
 angular.module('cylon')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
   var main= this;
+        main.robotname = null;
+        main._fetchRobots = function(){
+           $http({url:"http://127.0.0.1/Robots",method:"GET"}).success(function(result){
+               main.robotname = result[0].name;
+           });
+        };
         main.ChangeColor = function(color){
+            $http({url:"http://127.0.0.1:4321/", data:color}).success(function(){
+                console.log("Recieved")
+            });
             console.log("Sending "+color+"....");
         }
     });
